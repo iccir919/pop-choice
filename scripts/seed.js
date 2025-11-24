02c2893e787b6e8ce7fd625e9dd6df7b7e65d823
Neil <iccir919@gmail.com>
2025-11-24 13:26:00 -0800

    Create seed file for embedding and inserting data

--- scripts/seed.js
+++ scripts/seed.js
@@ -9,7 +9,13 @@ import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters'
     Return LangChain's "output" - the array of Document objects
 */
 async function splitText(text) {
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 250,
+        chunkOverlap: 35
+    })
 
+    const output = await splitter.createDocuments([text])
+    return output;
 }
 
 /*
@@ -20,7 +26,24 @@ async function createAndStoreEmbeddings() {
     const movieContent = movies
         .map((movie) => movie.content)
         .join("\n");
-    console.log(movieContent)
+    const chunkData = await splitText(movieContent);
+
+    const data = await Promise.all(
+        chunkData.map(async (chunk) => {
+            const embeddingResponse = await openai.embeddings.create({
+                model: "text-embedding-ada-002",
+                input: chunk.pageContent
+            });
+
+            return {
+                content: chunk.pageContent,
+                embedding: embeddingResponse.data[0].embedding
+            }
+        })
+    );
+
+    await supabase.from("aronofsky_movies").insert(data);
+    console.log('Embedding and storing complete!');
 }
 
 await createAndStoreEmbeddings()
